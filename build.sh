e45f028a7167829e97a6f06b40a26153ca279f19
Marcel Braun <contact@thatmarcel.com>
2025-01-26 18:38:38 +0100

    Update Tailwind CLI npx package name

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ sed -i "s/{{ AppVersion }}/$(cat latest-app-version.txt)/g" ./src/index.html
 rm -rf dist
 cp -R src dist
 cd src
-npx @tailwindcss/cl -i ./input.css -o ../dist/generated.css
+npx @tailwindcss/cli -i ./input.css -o ../dist/generated.css

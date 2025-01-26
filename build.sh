0ae0de2ab20ab478d1ec374862de11f967e589d4
thatmarcel <contact@thatmarcel.com>
2025-01-26 18:58:11 +0100

    Complete upgrade to Tailwind v4

--- build.sh
+++ build.sh
@@ -5,4 +5,6 @@ sed -i "s/{{ AppVersion }}/$(cat latest-app-version.txt)/g" ./src/index.html
 rm -rf dist
 cp -R src dist
 cd src
-npx @tailwindcss/cli -i ./input.css -o ../dist/generated.css
+npm i tailwindcss
+npx @tailwindcss/cli -i ./input.css -o ../dist/generated.css --minify
+rm -rf node_modules ./package*.json ../dist/package*.json ../dist/input.css
